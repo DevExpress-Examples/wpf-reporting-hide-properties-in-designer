55e4e7427470493135d3e20964cedbfddc4f253a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HidePropertiesExample/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using DevExpress.XtraReports.Expressions;
+using DevExpress.XtraReports.UI;
+
+namespace HidePropertiesExample {
+    public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            // For XRLabel controls, hide the Background Color and Tag properties from the Expressions tab.
+            ExpressionBindingDescriptor.HidePropertyDescriptions(typeof(XRLabel), "BackColor", "Tag");
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ VB/HidePropertiesExample/Application.xaml.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Windows
+Imports DevExpress.XtraReports.Expressions
+Imports DevExpress.XtraReports.UI
+
+Namespace HidePropertiesExample
+    Partial Public Class App
+        Inherits Application
+
+        Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
+            ' For XRLabel controls, hide the Background Color and Tag properties from the Expressions tab.
+            ExpressionBindingDescriptor.HidePropertyDescriptions(GetType(XRLabel), "BackColor", "Tag")
+            MyBase.OnStartup(e)
+        End Sub
+    End Class
+End Namespace
