3fe4322c0fd22254264a2af5bde6fd4df68a5a15
DevExpressExampleBot <fakeEmail>
2023-01-30 16:17:39 +0400

    Source auto update [skip ci]

---
+++ VB/HidePropertiesExample/App.xaml.vb
@@ -0,0 +1,16 @@
+Imports System.Windows
+Imports DevExpress.XtraReports.Expressions
+Imports DevExpress.XtraReports.UI
+
+Namespace HidePropertiesExample
+
+    Public Partial Class App
+        Inherits Application
+
+        Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
+            ' For XRLabel controls, hide the Background Color and Tag properties from the Expressions tab.
+            ExpressionBindingDescriptor.HidePropertyDescriptions(GetType(XRLabel), "BackColor", "Tag")
+            MyBase.OnStartup(e)
+        End Sub
+    End Class
+End Namespace
