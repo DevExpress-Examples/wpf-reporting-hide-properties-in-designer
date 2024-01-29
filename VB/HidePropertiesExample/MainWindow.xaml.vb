4c99039229c2db4fa2eeac6eb66b6fae8c35eb12
DevExpressExampleBot <fakeEmail>
2024-01-29 14:56:21 +0400

    Source auto update [skip ci]

--- VB/HidePropertiesExample/MainWindow.xaml.vb
+++ VB/HidePropertiesExample/MainWindow.xaml.vb
@@ -1,48 +1,46 @@
-﻿Imports System
+Imports System
 Imports System.ComponentModel
 Imports System.Windows
 Imports DevExpress.XtraReports.UI
 Imports DevExpress.XtraReports.UserDesigner
 
-
 Namespace HidePropertiesExample
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			Dim report = New XtraReport()
-			' Handle the static FilterComponentProperties event to filter the Properties panel.
-			AddHandler XtraReport.FilterComponentProperties, AddressOf XtraReport_FilterComponentProperties
-			reportDesigner.OpenDocument(report)
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+            Dim report = New XtraReport()
+            ' Handle the static FilterComponentProperties event to filter the Properties panel.
+            AddHandler XtraReport.FilterComponentProperties, AddressOf HidePropertiesExample.MainWindow.XtraReport_FilterComponentProperties
+            Me.reportDesigner.OpenDocument(report)
+        End Sub
 
-		Private Shared Sub XtraReport_FilterComponentProperties(ByVal sender As Object, ByVal e As FilterComponentPropertiesEventArgs)
-			' Hide the Scripts property for all report elements.
-			HideProperty("Scripts", e)
+        Private Shared Sub XtraReport_FilterComponentProperties(ByVal sender As Object, ByVal e As FilterComponentPropertiesEventArgs)
+            ' Hide the Scripts property for all report elements.
+            Call HideProperty("Scripts", e)
+            ' Hide the ReportSource and ReportSourceUrl properties for subreports. 
+            If TypeOf e.Component Is XRSubreport Then
+                Call HideProperty("ReportSource", e)
+                Call HideProperty("ReportSourceUrl", e)
+            End If
 
-			' Hide the ReportSource and ReportSourceUrl properties for subreports. 
-			If TypeOf e.Component Is XRSubreport Then
-				HideProperty("ReportSource", e)
-				HideProperty("ReportSourceUrl", e)
-			End If
+            ' Hide the Name property for XRLabel controls.
+            If TypeOf e.Component Is XRLabel Then
+                Call HideProperty("Name", e)
+            End If
+        End Sub
 
-			' Hide the Name property for XRLabel controls.
-			If TypeOf e.Component Is XRLabel Then
-				HideProperty("Name", e)
-			End If
-		End Sub
-
-		Private Shared Sub HideProperty(ByVal propertyName As String, ByVal filterComponentProperties As FilterComponentPropertiesEventArgs)
-			Dim oldPropertyDescriptor As PropertyDescriptor = TryCast(filterComponentProperties.Properties(propertyName), PropertyDescriptor)
-			If oldPropertyDescriptor IsNot Nothing Then
-				' Substitute the current property descriptor with a custom one with the BrowsableAttribute.No attribute.
-				filterComponentProperties.Properties(propertyName) = TypeDescriptor.CreateProperty(oldPropertyDescriptor.ComponentType, oldPropertyDescriptor, New Attribute() { BrowsableAttribute.No })
-			Else
-				' If the property descriptor cannot be substituted, remove it from the Properties collection.
-				filterComponentProperties.Properties.Remove(propertyName)
-			End If
-		End Sub
-	End Class
+        Private Shared Sub HideProperty(ByVal propertyName As String, ByVal filterComponentProperties As FilterComponentPropertiesEventArgs)
+            Dim oldPropertyDescriptor As PropertyDescriptor = TryCast(filterComponentProperties.Properties(propertyName), PropertyDescriptor)
+            If oldPropertyDescriptor IsNot Nothing Then
+                ' Substitute the current property descriptor with a custom one with the BrowsableAttribute.No attribute.
+                filterComponentProperties.Properties(propertyName) = TypeDescriptor.CreateProperty(oldPropertyDescriptor.ComponentType, oldPropertyDescriptor, New Attribute() {BrowsableAttribute.No})
+            Else
+                ' If the property descriptor cannot be substituted, remove it from the Properties collection.
+                filterComponentProperties.Properties.Remove(propertyName)
+            End If
+        End Sub
+    End Class
 End Namespace
